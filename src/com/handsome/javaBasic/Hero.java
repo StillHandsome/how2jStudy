1972cae54a6010a28c2d94f33ec761377533f42f
stillHandsome <1363328153@qq.com>
2017-12-03 12:37:09 +0800

    delete test file and add javaBasic code

---
+++ src/com/handsome/javaBasic/Hero.java
@@ -0,0 +1,24 @@
+package com.handsome.javaBasic;
+
+public class Hero {
+    private String name;
+    private String skill;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setSkill(String skill) {
+        this.skill = skill;
+    }
+
+    public String getSkill() {
+        return skill;
+    }
+
+
+}
